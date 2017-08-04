2c30dce40223f67d4484915a8700272c441570fe
Louis G <louis.gourdeau@posteo.net>
2017-08-04 20:11:20 +0200

    Change log level to INFO

--- laima/main.py
+++ laima/main.py
@@ -33,7 +33,7 @@ import twitter_agent
 import util
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
